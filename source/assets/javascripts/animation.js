8124ef8b26392c56de41d85d91178c6d1ba759d9
Deyvid Ferrer <deyvidferrer02@gmail.com>
2017-12-22 21:21:43 -0300

    animation fixed and aligments 2

--- source/assets/javascripts/animation.js
+++ source/assets/javascripts/animation.js
@@ -3,15 +3,11 @@ $(document).ready(function() {
   window.sr = ScrollReveal({ reset: true });
 
   // Customizing a reveal set
-  sr.reveal('.deco-object ', { duration: 400 });
-  sr.reveal('.deco-object-1 ', { duration: 500 });
-  sr.reveal('.deco-object-4 ', { duration: 400 });
-  sr.reveal('.deco-object-5 ', { duration: 500 });
 
   sr.reveal('.text ', { duration: 700 });
   sr.reveal('.img-sec ', { duration: 400 });
   sr.reveal('.p-text ', { duration: 500 });
-  sr.reveal('.row ', { duration: 600 });
+
   sr.reveal('.row-container ', { duration: 700 });
 
   sr.reveal('.content ', { duration: 600 });
